9e9762b22f2ca1f223f30e0c9f4f4ba1cba95d95
ANYA1128 <league@WTS19.attlocal.net>
2018-04-06 18:28:04 -0700

    Unbirthday and SecretMessageBox

---
+++ src/Unbirthday.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+	String answer = JOptionPane.showInputDialog(null, "When is your birthday? (mm/dd)");
+	if(answer.equals("04/06")) {
+		JOptionPane.showMessageDialog(null, "Happy Birthday!");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "A Very Merry UNBirthday to you!");
+		
+	}
+	
+}
+}
